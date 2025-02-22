14ecd8cbd89b77faff8d2e06855e448a7c177213
Hiroyuki Onishi <onishi.hiroyuki@gmail.com>
2025-02-22 18:51:14 +0900

    add initial content script

---
+++ src/entry/content.ts
@@ -0,0 +1,10 @@
+async function listenLoadEvent() {
+  // console.log("listenLoadEvent");
+  unmutePlayerIfMuted();
+}
+
+async function unmutePlayerIfMuted() {
+  // TODO
+}
+
+window.addEventListener("load", listenLoadEvent);
